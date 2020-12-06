a82ab143e5da820f5fa5f4ffd0f7c4f2e5c0c0f5
Stacy Betts <stacyo23@yahoo.com>
2020-12-05 21:40:08 -0600

    added inquirer prompt/promise

--- app.js
+++ app.js
@@ -8,8 +8,91 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+
 const render = require("./lib/htmlRenderer");
 
+const roster =[]; 
+
+function employeeData() {
+    console.log("Initializing Team Contact Generator..."); 
+   inquirer.prompt([
+       {
+        type: "list", 
+        name: "role", 
+        message: "What is the role for this employee?",
+        choices: ["Manager", "Engineer", "Intern", "Exit/Save"]
+       }, 
+       {
+        type: "input", 
+        name: "name", 
+        message: "What is the employee's name?",
+       when: function(response) {
+           return(response.role !== "Exit/Save")
+       }
+        }, 
+       {
+        type: "input", 
+        name: "id", 
+        message: "What is the employee's id number?",
+        when: function(response) {
+            return(response.role !== "Exit/Save")
+        }
+       }, 
+       {
+        type: "input", 
+        name: "email", 
+        message: "What is the employee's email?",
+        when: function(response) {
+            return(response.role !== "Exit/Save")
+        }
+       }, 
+       {
+        type: "input", 
+        name: "github", 
+        message: "What is the Engineer's gitHub name?",
+        when: function(response) {
+            return(response.role === "Engineer")
+        }
+       }, 
+       {
+        type: "input", 
+        name: "school", 
+        message: "What school does the Intern attend?",
+        when: function(response) {
+            return(response.role === "Intern")
+        }
+       }, 
+       {
+        type: "input", 
+        name: "officeNumber", 
+        message: "What is the Manager's office number?",
+        when: function(response) {
+            return(response.role === "Manager")
+        }
+       }, 
+   ]).then(function (response) {
+    console.log(this.response=response); 
+    if(response.role === "Manager") {
+     const manager = new Manager(response.name, response.id, response.email, response.officeNumber); 
+     roster.push(manager); 
+     employeeData();
+    } else if (response.role === "Engineer") {
+        const engineer = new Engineer(response.name, response.id, response.email, response.github); 
+        roster.push(engineer); 
+        employeeData();
+    } else if (response.role === "Intern") {
+        const intern = new Intern(response.name, response.id, response.email, response.school); 
+        roster.push(intern); 
+        employeeData();
+    } else {
+        console.log("Saving data...")
+    }
+
+   }); 
+
+}
+
+employeeData(); 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
